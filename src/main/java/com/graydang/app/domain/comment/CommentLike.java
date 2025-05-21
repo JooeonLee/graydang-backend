42724c019461cb0a16c6fbb7a9e131b3cf16f216
JooeonLee <cacophony0609@gmail.com>
2025-05-21 17:23:50 +0900

    feat: 댓글 도메인 엔티티 맵핑

---
+++ src/main/java/com/graydang/app/domain/comment/CommentLike.java
@@ -0,0 +1,30 @@
+package com.graydang.app.domain.comment;
+
+import com.graydang.app.domain.user.model.User;
+import com.graydang.app.global.common.model.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "comment_like")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class CommentLike extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id", nullable = false)
+    private Comment comment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, length = 255)
+    private String status;
+}
